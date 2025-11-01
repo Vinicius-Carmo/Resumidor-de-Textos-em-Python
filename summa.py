7ca896954a335c95d2a93b375d609fcbe892e02e
Vinicius Resende do Carmo <viniciuslife123@gmail.com>
2025-10-31 23:55:37 -0300

    Atualizando dados
    Adicionado com sucesso.

--- summa.py
+++ summa.py
@@ -13,8 +13,7 @@ from PIL import Image
 import pytesseract
 from gtts import gTTS
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
+#pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 # -------------------- Recursos NLTK --------------------
 def nltk_recursos(recurso, download):
